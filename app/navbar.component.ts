9feb496d90774820011e64004a3a716e69be0415
jarirliu1986 <289374247@qq.com>
2016-09-05 17:13:37 -0400

    highlight the router

--- app/navbar.component.ts
+++ app/navbar.component.ts
@@ -1,5 +1,5 @@
 import {Component} from "angular2/core";
-import {ROUTER_DIRECTIVES} from "angular2/router";
+import {ROUTER_DIRECTIVES, Router} from "angular2/router";
 
 @Component({
     selector: 'navbar',
@@ -7,4 +7,12 @@ import {ROUTER_DIRECTIVES} from "angular2/router";
     directives: [ROUTER_DIRECTIVES]
 })
 
-export class NavBarComponent{}
+export class NavBarComponent{
+    constructor (private _router : Router){
+    }
+
+    isCurrentRoute(route){
+        var instruction = this._router.generate(route);
+        return this._router.isRouteActive(instruction);
+    }
+}
